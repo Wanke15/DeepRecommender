dd163efd9dc08f4ca86e2b7ad2f322a2eec4e5fb
jeff-wang <wangke@zuzuche.com>
2020-08-11 17:37:42 +0800

    do not use tf.keras.experimental.WideDeepModel

--- wide_and_deep/train.py
+++ wide_and_deep/train.py
@@ -1,8 +1,9 @@
+import pickle
+
 import tensorflow as tf
 import pandas as pd
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import OneHotEncoder
-import pickle
 
 
 # Define the column names for the data sets.
@@ -26,7 +27,6 @@ org_linear_inputs = df_train[CATEGORICAL_COLUMNS]
 
 enc = OneHotEncoder(handle_unknown='ignore')
 linear_inputs = enc.fit_transform(org_linear_inputs).todense()
-
 with open("./models/one_hot_encoder.b", 'wb') as f:
     pickle.dump(enc, f)
 
@@ -40,11 +40,11 @@ print('Start building  model...')
 epochs = 10
 
 linear_model = tf.keras.Sequential([
-    tf.keras.layers.Dense(10, input_shape=(linear_inputs.shape[1],), activation='relu'),
+    tf.keras.layers.Dense(10, input_shape=(linear_inputs.shape[1], ), name="linear_feature_layer", activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
-linear_model.compile('adagrad', 'binary_crossentropy', metrics=['accuracy'])
-linear_model.fit(linear_inputs, y, batch_size=32, epochs=epochs)
+linear_model.compile('adagrad', 'binary_crossentropy', metrics=[tf.keras.metrics.AUC()])
+# linear_model.fit(linear_inputs, y, batch_size=32, epochs=epochs)
 
 auc_score = roc_auc_score(y, linear_model.predict(linear_inputs))
 print("Pure wide auc: ", auc_score)
@@ -52,16 +52,23 @@ print("Pure wide auc: ", auc_score)
 dnn_model = tf.keras.Sequential(
     [
         tf.keras.layers.Dense(units=32),
-        tf.keras.layers.Dense(units=8),
+        tf.keras.layers.Dense(units=8, name="dnn_feature_layer"),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
-dnn_model.compile('rmsprop', 'binary_crossentropy', metrics=['accuracy'])
-dnn_model.fit(dnn_inputs, y, batch_size=32, epochs=epochs)
+dnn_model.compile('rmsprop', 'binary_crossentropy', metrics=[tf.keras.metrics.AUC()])
+# dnn_model.fit(dnn_inputs, y, batch_size=32, epochs=epochs)
 auc_score = roc_auc_score(y, dnn_model.predict(dnn_inputs))
 print("Pure deep auc: ", auc_score)
 
-combined_model = tf.keras.experimental.WideDeepModel(linear_model, dnn_model, activation="relu")
-combined_model.compile(optimizer=['sgd', 'adam'], loss='binary_crossentropy', metrics=['accuracy'])
+
+combined_features = tf.keras.layers.concatenate([linear_model.get_layer("linear_feature_layer").output,
+                                                 dnn_model.get_layer("dnn_feature_layer").output])
+outputs = tf.keras.layers.Dense(1, activation=tf.nn.sigmoid, name="combined_output")(combined_features)
+
+combined_model = tf.keras.models.Model(inputs=[*linear_model.inputs, *dnn_model.inputs], outputs=outputs)
+combined_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[tf.keras.metrics.Precision()])
+# combined_model.summary()
+
 combined_model.fit([linear_inputs, dnn_inputs], y, batch_size=64, epochs=epochs)
 
 y_pred = combined_model.predict([linear_inputs, dnn_inputs])
@@ -71,10 +78,8 @@ from sklearn.metrics import roc_auc_score
 auc_score = roc_auc_score(y, y_pred)
 print("Pure w&d auc: ", auc_score)
 
-
 print("Saving model...")
 model_json = combined_model.to_json(indent=4)
 with open("./models/wide_and_deep/w_d_keras.json", "w") as json_file:
     json_file.write(model_json)
 combined_model.save_weights("./models/wide_and_deep/w_d_keras")
-
