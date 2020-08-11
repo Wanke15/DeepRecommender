a6c0fb3cd0ad0bebb537047b5d188ac3087c6283
jeff-wang <wangke@zuzuche.com>
2020-08-11 09:17:27 +0800

    follow protocol-buffers Style Guide

--- grpc_service/wide_and_deep/client.py
+++ grpc_service/wide_and_deep/client.py
@@ -5,10 +5,10 @@ from grpc_service.wide_and_deep import wide_and_deep_pb2_grpc, wide_and_deep_pb2
 channel = grpc.insecure_channel('localhost:50051')
 stub = wide_and_deep_pb2_grpc.WideAndDeepGrpcStub(channel)
 
-demo_data1 = {"workClass": "Private", "education": "11th", "maritalStatus": "Never-married",
+demo_data1 = {"workclass": "Private", "education": "11th", "marital_status": "Never-married",
                  "occupation": "Machine-op-inspct", "relationship": "Own-child", "race": "Black",
-                 "gender": "Male", "nativeCountry": "United-States",
-              "age": 25, "educationNum": 7, "capitalGain": 1, "capitalLoss": 1, "hoursPerWeek": 40}
+                 "gender": "Male", "native_country": "United-States",
+              "age": 25, "education_num": 7, "capital_gain": 1, "capital_loss": 1, "hours_per_week": 40}
 
 adult = wide_and_deep_pb2.Adult(**demo_data1)
 print(adult)
